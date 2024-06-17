8adb7abd8665dfda67dbdd8071ed0c2ca0904c76
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2024-06-17 13:32:23 -0300

    Created Swagger Config File

--- src/main/java/com/thiagonascimento/gestordevagas/GestorDeVagasApplication.java
+++ src/main/java/com/thiagonascimento/gestordevagas/GestorDeVagasApplication.java
@@ -1,21 +1,9 @@
 package com.thiagonascimento.gestordevagas;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @io.swagger.v3.oas.annotations.info.Info(
-                title = "Gestor de Vagas",
-                version = "1.0",
-                description = "API para gestão de vagas de emprego"
-        )
-)
-@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestorDeVagasApplication {
     public static void main(String[] args) {
         SpringApplication.run(GestorDeVagasApplication.class, args);
---
+++ src/main/java/com/thiagonascimento/gestordevagas/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.thiagonascimento.gestordevagas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gestor de Vagas")
+                        .description("API para gestão de vagas de emprego")
+                        .version("1.0.0"))
+                .schemaRequirement("jwt_auth", createSecurityScheme());
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
+                .name("jwt_auth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+    }
+}
